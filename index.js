bfd47c808cee39957cf6e65934216aa84899dce6
rodiat-oluwaseun <yemirolayemi@gmail.com>
2024-01-09 11:12:55 +0000

    team.html created for the input of user to be displayed on the browser

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // link to page creation
-const generateTeam = require('./src/page-template');
+const render= require('./src/page-template.js');
 
 // team profiles
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern'); 
-
-// node modules 
-const fs = require('fs'); 
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js'); 
 const inquirer = require('inquirer');
+const path = require("path");
+const fs = require('fs'); 
+
+
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+// node modules 
 
 // team array
 const teamArray = []; 
 
 // start of manager prompts 
-const addManager = () => {
+const teamManager = () => {
     return inquirer.prompt ([
         {
             type: 'input',
@@ -91,7 +96,7 @@ const addEmployee = () => {
             type: 'list',
             name: 'role',
             message: "Please choose your employee's role",
-            choices: ['Engineer', 'Intern']
+            choices: ['Engineer', 'Intern','writeHtml']
         },
         {
             type: 'input',
@@ -196,25 +201,23 @@ const addEmployee = () => {
 
 
 // function to generate HTML page file using file system 
-// const writeFile = data => {
-   
-// }; 
-// function for building the html file
-const createHtml = (teamArray) => {
-  console.log("Your Team is Created!")
-  fs.writeFileSync(outputPath, render(teamArray), "UTF-8")
-}
-//INIT FUNCTION
-addManager();
 
-// addManager()
-//   .then(addEmployee)
-//   .then(teamArray => {
-//     return generateTeam(teamArray);
-//   })
-//   .then(pageHTML => {
-//     return writeFile(pageHTML);
-//   })
-//   .catch(err => {
-//  console.log(err);
-//   });
+
+const writeHtml = ()=> {
+    console.log("Your team profile has been successfully created! Please check out the team.html")
+    fs.writeFileSync(outputPath, render(teamArray),"UTF-8")  
+}; 
+
+teamManager()
+  .then(addEmployee)
+  .then(teamArray => {
+    return render(teamArray);
+  })
+  .then(pageHTML => {
+    return writeHtml(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
+
+  
